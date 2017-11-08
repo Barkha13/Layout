78ecabd94ceaf61721f70c22680c0ec55ae0c8f7
Barkha13 <barkha0292@yahoo.com>
2017-11-08 01:26:35 -0800

    first commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
